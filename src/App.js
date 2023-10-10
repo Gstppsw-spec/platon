1678f807355283847f1ab781ff4946728a088c90
Gstppsw-spec <gunawan.119140129@student.itera.ac.id>
2023-10-10 08:31:27 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,39 +1,120 @@
 import * as React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
+import { Nav } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import "bootstrap/dist/css/bootstrap.css";
-//import "./App.css";
-
-import { BrowserRouter as Router , Routes, Route, Link } from "react-router-dom";
-
-import EditProduct from "./components/product/edit.component";
-import ProductList from "./components/product/list.component";
-import CreateProduct from "./components/product/create.component";
+// import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 function App() {
-  return (<Router>
-    <Navbar bg="primary">
-      <Container>
-        <Link to={"/"} className="navbar-brand text-white">
-          TechvBlogs
-        </Link>
-      </Container>
-    </Navbar>
+  return (
+    <div className="App">
+      <form className="formTambahBarang">
+        <h2>Tambah Barang</h2>
+        <div className="container">
+          <fieldset className="fieldset">
+            <legend className="legendMerk">
+              Merk <span style={{ color: "red" }}>*</span>
+            </legend>
+            <select name="cars" id="cars" placeholder="Masukkan merk" className="inputMerk" >
+              <option value="">Masukkan merk</option>
+              <option value="saab">Saab</option>
+              <option value="mercedes">Mercedes</option>
+              <option value="audi">Audi</option>
+            </select>
+          </fieldset>
+          <fieldset className="fieldset">
+          <legend className="legendMerk">
+              Kode <span style={{ color: "red" }}>*</span>
+            </legend>
+            <select name="cars" id="cars" placeholder="Masukkan Kode" className="inputMerk" >
+              <option value="">Masukkan kode</option>
+              <option value="saab">Saab</option>
+              <option value="mercedes">Mercedes</option>
+              <option value="audi">Audi</option>
+            </select>
+          </fieldset>
+        </div>
 
-    <Container className="mt-5">
-      <Row>
-        <Col md={12}>
-          <Routes>
-            <Route path="/product/create" element={<CreateProduct />} />
-            <Route path="/product/edit/:id" element={<EditProduct />} />
-            <Route exact path='/' element={<ProductList />} />
-          </Routes>
-        </Col>
-      </Row>
-    </Container>
-  </Router>);
+        <div className="container">
+          <fieldset className="fieldset">
+            <legend className="legendMerk">
+              Seri <span style={{ color: "red" }}>*</span>
+            </legend>
+            <select name="cars" id="cars" placeholder="Masukkan Seri" className="inputMerk" >
+              <option value="">Masukkan seri</option>
+              <option value="saab">Saab</option>
+              <option value="mercedes">Mercedes</option>
+              <option value="audi">Audi</option>
+            </select>
+          </fieldset>
+          <fieldset className="fieldset">
+          <legend className="legendMerk">
+              Tipe <span style={{ color: "red" }}>*</span>
+            </legend>
+            <select name="cars" id="cars" placeholder="Masukkan Tipe" className="inputMerk" >
+              <option value="">Masukkan tipe</option>
+              <option value="saab">Saab</option>
+              <option value="mercedes">Mercedes</option>
+              <option value="audi">Audi</option>
+            </select>
+          </fieldset>
+        </div>
+
+        <div className="container">
+          <fieldset className="fieldset">
+            <legend className="legendMerk">
+              Kategori <span style={{ color: "red" }}>*</span>
+            </legend>
+            <select name="cars" id="cars" placeholder="Masukkan kategori" className="inputMerk" >
+              <option value="">Masukkan kategori</option>
+              <option value="saab">Saab</option>
+              <option value="mercedes">Mercedes</option>
+              <option value="audi">Audi</option>
+            </select>
+          </fieldset>
+          <fieldset className="fieldset">
+          
+
+            <legend className="legendKode">
+              Nama <span style={{ color: "red" }}>*</span>
+            </legend>
+            <input
+              type="text"
+              placeholder="Masukkan nama"
+              className="inputMerk"
+            />
+          </fieldset>
+        </div>
+        <div className="container">
+          <fieldset className="fieldset">
+            <legend className="legendMerk">
+              Sparepart <span style={{ color: "red" }}>*</span>
+            </legend>
+            <select name="cars" id="cars" placeholder="Masukkan Sparepart" className="inputMerk" >
+              <option value="">Masukkan sparepart</option>
+              <option value="saab">Saab</option>
+              <option value="mercedes">Mercedes</option>
+              <option value="audi">Audi</option>
+            </select>
+          </fieldset>
+          <fieldset className="fieldset">
+          <legend className="legendMerk">
+              Satuan <span style={{ color: "red" }}>*</span>
+            </legend>
+            <select name="cars" id="cars" placeholder="Masukkan Satuan" className="inputMerk" >
+              <option value="">Masukkan satuan</option>
+              <option value="saab">Saab</option>
+              <option value="mercedes">Mercedes</option>
+              <option value="audi">Audi</option>
+            </select>
+          </fieldset>
+        </div>
+        <button type="submit">Tambahkan</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
